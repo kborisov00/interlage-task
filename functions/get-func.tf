4edb6797f8295cc1c97adfc1adf71ebb52cd266a
Kristiyan Borisov <genkoph@protonmail.com>
2022-07-27 18:09:41 +0300

    feat: configure api gateway for each function

--- functions/get-func.tf
+++ functions/get-func.tf
@@ -12,3 +12,39 @@ resource "aws_lambda_function" "lambda_get_func" {
   runtime          = "nodejs14.x"
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
+
+resource "aws_apigatewayv2_api" "apigateway_get_func" {
+  name          = "apigateway_get_func"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "apigateway_stage_get_func" {
+  api_id = aws_apigatewayv2_api.apigateway_get_func.id
+
+  name        = "apigateway_stage_get_func"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "apigateway_integration_get_func" {
+  api_id = aws_apigatewayv2_api.apigateway_get_func.id
+
+  integration_uri    = aws_lambda_function.lambda_get_func.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "apigateway_route_get_func" {
+  api_id = aws_apigatewayv2_api.apigateway_get_func.id
+
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.apigateway_integration_get_func.id}"
+}
+
+resource "aws_lambda_permission" "api_gw_get_func" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_get_func.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.apigateway_get_func.execution_arn}/*/*"
+}
