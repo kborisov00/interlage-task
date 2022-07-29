fab8b1894a859133c38990bb5133c7f5d52f0310
Kristiyan Borisov <genkoph@protonmail.com>
2022-07-29 14:47:53 +0300

    feat: add generic styles

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+import "styles/reset.css";
+
+const StyledCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 100vw;
+  overflow-x: hidden;
+  background-color: red;
+`;
 
 function App() {
-  return (
-    <div className="App">Hello, World!</div>
-  );
+  return <StyledCenter>Hello, World!</StyledCenter>;
 }
 
 export default App;
